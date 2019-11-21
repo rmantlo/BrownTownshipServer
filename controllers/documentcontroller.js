d3487370e12ffde9261f71be6d8a93294d6be0cc
Becky <you@example.com>
2019-11-21 15:17:57 -0500

    search by date and type

--- controllers/documentcontroller.js
+++ controllers/documentcontroller.js
@@ -29,17 +29,63 @@ router.get('/search/:documentType', (req, res) => {
 })
 //search by date (with surrounding dates)
 router.get('/searchdate/:fileDate', (req, res) => {
-    let date = req.params.fileDate;
-    console.log(new Date(date).toLocaleDateString())
+    let date = new Date(req.params.fileDate);
+    console.log(date);
+    let futureDate = new Date(new Date(req.params.fileDate).setDate(date.getDate() + 5));
+    console.log(futureDate)
+    let data = {
+        docsBeforeDate: [],
+        docsAfterDate: []
+    }
     Budget.findAll({
         where: {
             fileDate: {
-                [Op.lte]: new Date(date)
+                [Op.lte]: futureDate
             }
         }
     }, { order: [['fileDate', 'DESC']] })
-    .then(info => res.status(200).json(info))
-    .catch(err => res.sendStatus(500).json(err))
+        .then(info => {
+            info.forEach(d => {
+                if (new Date(d.fileDate) <= date) {
+                    data.docsBeforeDate.push(d)
+                }
+                else {
+                    data.docsAfterDate.push(d);
+                }
+            })
+        })
+        .then(info => res.status(200).json(data))
+        .catch(err => res.sendStatus(500).json(err))
+})
+//search by both date and type
+router.post('/searchtypedate', (req, res) => {
+    let date = new Date(req.body.date);
+    let futureDate = new Date(new Date(req.body.date).setDate(date.getDate() + 5));
+    let data = {
+        docsBeforeDate: [],
+        docsAfterDate: []
+    }
+    Budget.findAll({
+        where: {
+            documentType: req.body.type,
+            fileDate: {
+                [Op.lte]: futureDate
+            }
+        }
+    },
+        { order: [['fileDate', 'DESC']] })
+        .then(info => {
+            info.forEach(d => {
+                if (new Date(d.fileDate) <= date) {
+                    data.docsBeforeDate.push(d)
+                }
+                else {
+                    data.docsAfterDate.push(d);
+                }
+            })
+        })
+        .then(info => res.status(200).json(data))
+        .catch(err => res.status(500).json(err))
 })
 
 module.exports = router;
